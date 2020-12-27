69f8c242adbc4b455c265e48efc665a4cafe8cd4
Govorun11 <edik0076207@gmail.com>
2020-12-27 23:04:13 +0200

    qwerty

---
+++ 26_task1.py
@@ -0,0 +1,17 @@
+'''
+The greatest number
+
+Write a Python program to get the largest number from a list of random numbers with the length of 10
+
+Constraints: use only while loop and random module to generate numbers
+'''
+import  random
+
+my_list = []
+i = 0
+while i <= 10:
+    x = random.randint(0, 10)
+    my_list.append(x)
+    i+=1
+else:
+    print(my_list)
