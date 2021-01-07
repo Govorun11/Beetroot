42a120dfc5df869bc776e51f1088367fd13ae72c
Govorun11 <edik0076207@gmail.com>
2021-01-07 15:57:52 +0200

    qwerty

--- lesson7(30-12-2020).py
+++ lesson7(30-12-2020).py
@@ -33,3 +33,43 @@ def make_country(name, capital): #add a key 'name', key 'capital' for dict count
 print(make_country(country_name, country_capital))
 print(country.values())
 
+'''
+TASK 3
+
+Create a function called make_operation, which takes in a simple arithmetic operator as a first parameter
+
+(to keep things simple let it only be ‘+’, ‘-’ or ‘*’)
+
+and an arbitrary number of arguments (only numbers) as the second parameter.
+
+Then return the sum or product of all the numbers in the arbitrary parameter. For example:
+
+    the call make_operation(‘+’, 7, 7, 2) should return 16
+    the call make_operation(‘-’, 5, 5, -10, -20) should return 30
+    the call make_operation(‘*’, 7, 6) should return 42
+
+'''
+
+
+def make_operation(operation, *args):
+    if operation in ('+', '-', '*'):
+        summa = 0
+        mul = 1
+        if operation == '+':
+            summa = 0
+            for number in args:
+                summa += number
+            return summa
+        elif operation == '-':
+            summa = args[0]
+            for number in args[1:]:
+                summa -= number
+            return summa
+
+        else:
+            for number in args:
+                mul *= number
+            return mul
+
+
+print(make_operation('-', 5, 5, -10, -20))
